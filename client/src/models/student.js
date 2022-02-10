2e629047fc643eea916c0c3d17080d718b03de4a
Francisco Junior <fininhors@gmail.com>
2022-02-10 07:25:11 -0300

    Client - Definição da camada de modelos

---
+++ client/src/models/student.js
@@ -0,0 +1,8 @@
+export default class Student {
+    constructor(name, email, ra, cpf) {
+        this.name = name;
+        this.email = email;
+        this.ra = ra;
+        this.cpf = cpf;
+    }
+}
